87643be07f8ce77c229c37306be071d4758de76a
kunasyl <kunasyl45@gmail.com>
2023-10-01 23:37:33 +0600

    hw3

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+
+
+const withLifecycleLogging = (WrappedComponent) => {
+  return class extends React.Component {
+    componentDidMount() {
+      console.log(`Component ${WrappedComponent.name} mounted`);
+    }
+
+    componentWillUnmount() {
+      console.log(`Component ${WrappedComponent.name} will unmount`);
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+};
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Component did mount or update');
+    return () => {
+      console.log('Component will unmount');
+    };
+  }, [count]);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Вы кликнули {count} раз</p>
+      <button onClick={increment}>
+        Нажми на меня
+      </button>
+    </div>
+  );
+}
+
+const LoggedCounter = withLifecycleLogging(Counter);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LoggedCounter />
     </div>
   );
 }
